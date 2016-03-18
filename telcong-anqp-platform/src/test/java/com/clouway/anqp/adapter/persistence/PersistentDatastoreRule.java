58d68a731c9647c1ffa9be55fe73a5d196fdaf4d
freekman <ivan.genchev1989@gmail.com>
2016-03-18 13:38:32 +0200

    Revert the PersistentDatastoreRule to work with the embeded mongo

--- telcong-anqp-platform/src/test/java/com/clouway/anqp/adapter/persistence/PersistentDatastoreRule.java
+++ telcong-anqp-platform/src/test/java/com/clouway/anqp/adapter/persistence/PersistentDatastoreRule.java
@@ -1,31 +1,55 @@
 package com.clouway.anqp.adapter.persistence;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import org.junit.rules.ExternalResource;
 
 public final class PersistentDatastoreRule extends ExternalResource {
-  private static final String DATABASE_NAME = "anqp_e2e_test";
-  private static final Integer port = 27017;
-
+  private MongodExecutable mongodExecutable;
+  private MongodProcess mongodProcess;
   private MongoDatabase db;
+
+  private final String localhost = "localhost";
+  private final Integer port = 12345;
 
   @Override
   protected void before() throws Throwable {
-    MongoClient mongo = new MongoClient("dev.telcong.com", port);
+    MongodStarter runtime = MongodStarter.getDefaultInstance();
 
-    db = mongo.getDatabase(DATABASE_NAME);
+    mongodExecutable = runtime.prepare(new MongodConfigBuilder()
+            .version(Version.Main.PRODUCTION)
+            .net(new Net(port, Network.localhostIsIPv6()))
+            .build());
+
+    mongodProcess = mongodExecutable.start();
+
+    ServerAddress address = new ServerAddress(localhost, port);
+
+    MongoClient mongo = new MongoClient(address, MongoClientOptions.builder().connectionsPerHost(2).build());
+    db = mongo.getDatabase("anqp_test");
   }
 
-  public String getHostName() {
-    return "dev.telcong.com:" + port;
-  }
-
-  public String getDatabaseName() {
-    return DATABASE_NAME;
+  @Override
+  protected void after() {
+    mongodProcess.stop();
+    mongodExecutable.stop();
   }
 
   public MongoDatabase db() {
     return db;
   }
+
+  public String getConnectionURL() {
+    return localhost + ":" + port;
+  }
+
 }
